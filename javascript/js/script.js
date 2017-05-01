39261284dbe84c6c33189f0a92ce08932f4897fd
Timothy Kim <tkim1202@gmail.com>
2017-05-01 14:51:04 +0200

    Added javascript exercises

---
+++ javascript/js/script.js
@@ -0,0 +1,26 @@
+console.log("This is from an external file...");
+
+var valye = null;
+var type = null;
+
+value = "123";
+console.log(value);
+type = typeof value;
+console.log(type);
+
+'123'
+
+function hello() {
+    console.log("Hello World");
+}
+
+function say_hello_to(whom) {
+    console.log("Hello, " + whom);
+}
+
+function print_type (input) {
+    type = typeof input;
+    console.log("The type of " + input + " is a " + type)
+}
+
+print_type(game)
